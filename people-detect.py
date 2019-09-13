075c0e43e9249d5a11a43fbcec497b0eb94373f7
Tom <48766856+humandecoded@users.noreply.github.com>
2019-09-13 00:41:12 -0400

    addied twilio texting functionality

--- people-detect.py
+++ people-detect.py
@@ -1,15 +1,12 @@
 
-""" To do: 
--Add in text message alert functionality with twilio, option passed via flag --twilio """
-
-
-
-
 import cvlib    #high level module, uses YOLO model with the find_common_objects method
 import cv2      #image/video manipulation, allows us to pass frames to cvlib
 from argparse import ArgumentParser
 import os
+import sys
 from datetime import datetime
+from twilio.rest import Client  #used for texting if you'd like, flag is optional, 
+
 
 #function takes a file name, checks that file for human objects
 def humanChecker(video_name):
@@ -53,12 +50,33 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument('-d', '--directory', required=True, help='Path to video folder')
+    parser.add_argument('--twilio', action='store_true', help='Flag to use Twilio text notification')
     args = vars(parser.parse_args())
+    
+    #if the --twilio flag is used, this will look for environmental variables holding this needed information
+    #you can hardcode this information here if you'd like though. It's less secure but if you're the only one
+    #using this script it's probably fine
+    if args['twilio']:
+        try:
+            TWILIO_TOKEN = os.environ['TWILIO_TOKEN']
+            TWILIO_SID = os.environ['TWILIO_SID']
+            TWILIO_FROM = os.environ['TWILIO_FROM']
+            TWILIO_TO = os.environ['TWILIO_TO']
+        except:
+            print('Something went wrong with the Twilio variables. Either set your environmental variables or hardcode values in to script: TWILIO_TOKEN, TWILIO_SID, TWILIO_FROM, TWILIO_TO')
+            sys.exit(1)
 
     #create our log file and write the file names where we detect people
-    with open(datetime.now().strftime('%m%d%Y-%H:%M:%S') + '.txt', 'w') as w:
+    time_stamp = datetime.now().strftime('%m%d%Y-%H:%M:%S') + '.txt'
+    with open(time_stamp, 'w') as log_file:
         for current_file in getListOfFiles(args['directory'] + '/'):
             print(f'Working on {current_file}')
             if humanChecker(str(current_file)):
                 print(f'Human detected in {current_file}')
-                w.write(f'omg. intruder alert in {current_file} \n' )
+                log_file.write(f'omg. intruder alert in {current_file} \n' )
+    #if people are detected and --twilio flag has been set, send a text
+    with open(time_stamp, 'r') as log_file:
+        if log_file.readline() != '' and args['twilio']:
+            client = Client(TWILIO_SID, TWILIO_TOKEN)
+            client.messages.create(body="Human Detected. Check log files", from_=TWILIO_FROM, to=TWILIO_TO)
+        
